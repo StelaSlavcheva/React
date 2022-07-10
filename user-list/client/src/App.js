934e28589ead520f94b8b346c02c2ad8fbd4e7c6
Stela Slavcheva <slavcheva2711@gmail.com>
2022-07-10 22:52:30 +0300

    user-list-add new users

--- user-list/client/src/App.js
+++ user-list/client/src/App.js
@@ -2,18 +2,31 @@ import "./App.css";
 import { Footer } from "./components/common/Footer";
 import { Header } from "./components/common/Header";
 import { Search } from "./components/search/Search";
-import { UserSection } from "./components/user-section/UserSection";
+import { UserList} from "./components/user-list/UserList";
+import {useState, useEffect} from 'react';
+
+const baseUrl = 'http://localhost:3005/api'
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+      fetch (`${baseUrl}/users`)
+      .then(response => response.json())
+      .then(result =>{
+        setUsers(result.users);
+      })
+  }, []);
+
+  console.log(users);
+
   return (
     <div className="App">
       <Header />
       <main className="main">
         <section className="card users-container">
           <Search />
-          <UserSection	/>
-
-            
+          <UserList	users={users}/>            
         </section>
       </main>
       <Footer />
